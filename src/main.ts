d1318533a3da79162ca5565c8ccb148084defef7
godenm <1445546805@qq.com>
2024-02-19 16:00:34 +0800

    feat: commit1

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// 配置国际化
+import { zhCn } from 'element-plus/es/locales.mjs'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus, {
+  locale: zhCn, // element-plus国际化的配置
+})
 
-// const fun = () => {
-//   console.log('husky test!!')
-// }
+// 获取环境变量
+console.log(import.meta.env)
 
-// const fun1 = () => {
-//   console.log('husky test2!!')
-// }
+app.mount('#app')
