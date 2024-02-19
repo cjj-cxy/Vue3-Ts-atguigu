7a973120a2c01d642310db7df28e5fc08065d864
godenm <1445546805@qq.com>
2024-02-19 11:39:20 +0800

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <template>
-  <h1>hello World
-  </h1>
+  <h1>hello World</h1>
 </template>
 
-<script lang="ts" setup name="">
-
-</script>
+<script lang="ts" setup name=""></script>
 
 <style scoped></style>
