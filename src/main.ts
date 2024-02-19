8d6871e1986dace5380352fe316db6d58efbb74c
godenm <1445546805@qq.com>
2024-02-19 13:51:08 +0800

    format test

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,13 @@ import App from './App.vue'
 
 createApp(App).mount('#app')
 
-const fun = () => 
-{
-    console.log("husky test!!")
-    
+const fun = () => {
+  console.log('husky test!!')
 }
+
+
+const fun1 = () => 
+{
+
+    console.log('husky test2!!')
+  }
